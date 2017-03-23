921df40d1a38215ce105ac1a45fe5c7a6a7eb074
lili <li012589li@163.com>
2017-03-23 16:30:16 +0800

    include data used & initilaize global vars

--- prediction.py
+++ prediction.py
@@ -6,3 +6,10 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.model_selection import cross_val_score
 
+base_elo = 1600
+team_elo = {}
+team_stats = {}
+X = []
+y = []
+folder = 'data'
+
